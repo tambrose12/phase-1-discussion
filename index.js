783ea713ad1f567d9aa065d92f772690497bb421
Taylor Ambrose <taykesby@gmail.com>
2023-01-31 11:59:38 -0500

    adding all files

---
+++ index.js
@@ -0,0 +1,13 @@
+const h2 = document.createElement("h2");
+h2.textContent = "Hello, World!";
+
+document.querySelector("body").appendChild(h2);
+
+
+
+
+let myArray = ['a', 'b', 'c', 'd', 'f'];
+
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
